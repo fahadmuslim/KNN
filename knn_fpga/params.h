a472008084a0af5168a1ec22f81a4f71d2666397
fahadmuslim <fahadbinmuslim@gmail.com>
2016-07-04 11:50:42 +0200

    Update params.h
    Global size increased from 32k

--- knn_fpga/params.h
+++ knn_fpga/params.h
@@ -20,7 +20,7 @@
 */
 #define NUM_NEIGHBORS 5 // number of neighbors to be returned
 #define WORK_GROUP_SIZE 256
-#define GLOBAL_SIZE 32768 // size of global memory buffer
+#define GLOBAL_SIZE 294912 // size of global memory buffer
 #define REC_LENGTH 49 // size of a record in the input file
 #define QUERY_LAT 30.0 // query point latitude
 #define QUERY_LNG 90.0 // query point longitude
